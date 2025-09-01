d81ba05e003bc18c12cfd691615f746a4aef717f
omar <omarqais225@gmail.com>
2025-09-01 22:05:52 +0300

    route view

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [JobController::class, 'home']);
+//Route::get('/', [JobController::class, 'home']);
+//Route::get('/contact', [JobController::class, 'contact']);
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 Route::get('/jobs/create', [JobController::class, 'create']);
 Route::get('/jobs', [JobController::class, 'index']);
 Route::get('/job/{job}', [JobController::class, 'show']);
-Route::get('/contact', [JobController::class, 'contact']);
 Route::post('/jobs', [JobController::class, 'store']);
 Route::get('/job/{job}/edit', [JobController::class, 'edit']);
 Route::patch('/jobs/{job}/update', [JobController::class, 'update']);
