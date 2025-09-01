dbd04cab863d9db1a7fee20ccaa284869f799738
omar <omarqais225@gmail.com>
2025-09-01 22:09:32 +0300

    group routes

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,16 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', [JobController::class, 'home']);
-//Route::get('/contact', [JobController::class, 'contact']);
-Route::view('/', 'home');
-Route::view('/contact', 'contact');
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/job/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/job/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}/update', [JobController::class, 'update']);
-Route::delete('/job/{job}', [JobController::class, 'destroy']);
+Route::controller(JobController::class)->group(function () {
+    //Route::get('/',  'home');
+    //Route::get('/contact',  'contact');
+    Route::view('/', 'home');
+    Route::view('/contact', 'contact');
+    Route::get('/jobs/create',  'create');
+    Route::get('/jobs',  'index');
+    Route::get('/job/{job}',  'show');
+    Route::post('/jobs',  'store');
+    Route::get('/job/{job}/edit',  'edit');
+    Route::patch('/jobs/{job}/update',  'update');
+    Route::delete('/job/{job}',  'destroy');
+});
