5a713f4f0112b73bf2af139f04fbc46e5a3aeb54
omar <omarqais225@gmail.com>
2025-09-01 20:44:38 +0300

    form csrf request fillable guraded post

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,27 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => Job::with('employer')->cursorPaginate(5)]);
+    return view('jobs.index', ['jobs' => Job::with('employer')->latest()->paginate(5)]);
 });
 
 Route::get('/job/{id}', function ($id) {
     $job = Job::find($id);
     if (!$job) abort(404);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    $newJob = Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
